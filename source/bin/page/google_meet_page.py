4775ff98df7cb70b88f3ac83d2b06ea3ee85c726
Grendaizer <alexey.pro2015@gmail.com>
2022-10-05 18:04:06 +0300

    updating bot implementation

--- source/bin/page/google_meet_page.py
+++ source/bin/page/google_meet_page.py
@@ -1,7 +1,9 @@
 import time
+import pyautogui
 from source.bin.settings.config import URL
 from selenium.webdriver.common.by import By
 from source.bin.settings.config import CODE
+
 
 class meet_page:
     button = '/html/body/header/div[1]/div/div[3]/div[1]/div/span[1]/a'
@@ -9,7 +11,7 @@ class meet_page:
     confirm_button = '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/button/span'
     microphone = '//*[@id="ow3"]/div[1]/div/div[10]/div[3]/div[10]/div[2]/div/div[1]/div/div/span/button/div[3]/div[1]/span[2]/svg'
     video = '/html/body/div[1]/c-wiz/div/div/div[10]/div[3]/div/div[1]/div[4]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div[1]'
-    connect = '/html/body/div[1]/c-wiz/div/div/div[10]/div[3]/div/div[1]/div[4]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/button/span'
+    connect = 'span.VfPpkd-vQzf8d:nth-child(4)'
 
     def __init__(self, driver):
         self.driver = driver
@@ -29,8 +31,12 @@ class meet_page:
         time.sleep(5)
 
     def confirm_meet(self):
-        con = self.driver.find_element(By.XPATH, self.connect)
+        con = self.driver.find_element(By.CSS_SELECTOR, self.connect)
         con.click()
 
     def write_plus_in_chat(self):
         pass
+
+    def off_micro(self):
+        pyautogui.hotkey('ctrl', 'd')
+        pyautogui.hotkey('ctrl', 'e')
